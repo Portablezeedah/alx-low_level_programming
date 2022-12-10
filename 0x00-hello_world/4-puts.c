28507dd7b9b15a90f60347946178bbcb5f616db5
Mozeedat Adediran <adediranmozeedat@gmail.com>
2022-12-10 11:36:00 -0800

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Entry point
- *
+ * description - prints: Programming is like building a multilingual puzzle
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
